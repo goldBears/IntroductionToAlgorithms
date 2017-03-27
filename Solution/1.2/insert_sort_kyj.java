56f9d2a41acc5ad2f65fb7fcf5108ad69653e882
Kim yongjin <yongjin5184@gmail.com>
2017-03-28 08:54:04 +0900

    update indentation

--- Solution/1.2/insert_sort_kyj.java
+++ Solution/1.2/insert_sort_kyj.java
@@ -11,22 +11,21 @@ class Ideone{
   
 		int [] arr = {5, 2, 4, 6, 1, 3}; //
     		
-    for(int i = 1; i < arr.length; i++) {
+    		for(int i = 1; i < arr.length; i++) {
 			int standard = arr[i];  //sorting 시작값
-      int index = i - 1;   // 좌측두번째부터
+      			int index = i - 1;   // 좌측두번째부터
 			
-			while(index >= 0 && arr[index] > standard ) //오름차순
-			{				
-        arr[index + 1] = arr[index];   // 이동				
-        index--;
+			while(index >= 0 && arr[index] > standard ){ //오름차순
+        			arr[index + 1] = arr[index];   // 이동				
+        			index--;
 			}
       
 			arr[index + 1] = standard; 
 		}
 		
 		for(int i = 0; i < arr.length; i++)
-			System.out.print(arr[i] + " "); //
-	}
+			System.out.print(arr[i] + " "); //출력	
+}
 
 	
 }
